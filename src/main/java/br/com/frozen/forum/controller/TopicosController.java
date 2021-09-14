28a18e19ca9c2224c7361fd4c1771d83d5f13db1
ruth.gomes <ruthfgms@gmail.com>
2021-09-14 11:36:59 -0300

    refactor: changed annotations @Controller to @RestController to avoid use @ResponseBody always

--- src/main/java/br/com/frozen/forum/controller/TopicosController.java
+++ src/main/java/br/com/frozen/forum/controller/TopicosController.java
@@ -1,23 +1,23 @@
 package br.com.frozen.forum.controller;
 
+import br.com.frozen.forum.controller.dto.TopicoDto;
 import br.com.frozen.forum.model.Curso;
 import br.com.frozen.forum.model.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
         Topico topico = new Topico("Dúvida", "Não sei de nada", new Curso("Spring", "Programação"));
 
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 
     }
 }
