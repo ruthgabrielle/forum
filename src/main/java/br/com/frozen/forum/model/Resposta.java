e676634ace51d8a7b3093068b5bd5be404cca0cb
ruth.gomes <ruthfgms@gmail.com>
2021-09-20 14:38:28 -0300

    chore: config JPA

--- src/main/java/br/com/frozen/forum/model/Resposta.java
+++ src/main/java/br/com/frozen/forum/model/Resposta.java
@@ -1,13 +1,19 @@
 package br.com.frozen.forum.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
